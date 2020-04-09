563139d7343b8821a0cdddef7eee3cd6d82d0ba7
andreendo <andreendo@gmail.com>
2020-04-09 00:46:15 -0300

    some updates

--- events-data.js
+++ events-data.js
@@ -1,5 +1,11 @@
 var events = [
     {
+        id: "AST 2020",
+        link: "https://conf.researchr.org/home/icse-2020/ast-2020",
+        place: "Seoul, South Korea",
+        role: "PC member"
+    },
+    {
         id: "SBES 2019",
         link: "https://cbsoft2019.ufba.br/#/sbes",
         place: "Salvador, Brazil",
